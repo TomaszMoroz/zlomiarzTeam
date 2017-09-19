df42a817790178719319f1f93a3672549f7fbbea
TomaszMoroz <tmoroz688@gmail.com>
2017-09-19 09:38:28 +0200

    git clean up

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+	//open-close submenu on mobile
+	$('.cd-main-nav').on('click', function(event){
+		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
+	});
+});
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
