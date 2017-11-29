f9255a9be0cca0034fa5a2b491c0ffc105a5ca21
TomaszMoroz <tmoroz688@gmail.com>
2017-11-29 12:37:03 +0100

    new js code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($){
-	// open-close submenu on mobile
+	// obsluga rozwijanego menu na mobile
 	$('.cd-main-nav').on('click', function(event){
 		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
 
-    // nav click -> scroll to section
+    // gladkie przewijanie
     $('.cd-header a').on('click', function(){
         $('html, body').animate({
             scrollTop: $( $(this).attr('href') ).offset().top
@@ -12,3 +12,32 @@ jQuery(document).ready(function($){
         return false;
     });
 });
+
+    // obsluga google maps api
+    function initMap() {
+        var zlomiarzTeam = {lat: 54.380737, lng: 18.595791};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: zlomiarzTeam
+        });
+        var marker = new google.maps.Marker({
+          position: zlomiarzTeam,
+          map: map
+        });
+      }  
+
+      // podmiana opisu na imie w sekcji trenerow
+
+
+    $(".trainer-img").click(function() {
+        
+        $(this).siblings().slideToggle(1000).toggleClass("name");
+       
+        
+        });
+
+        // email po wcisnieciu ikony
+
+        $('#email').click(()=> {
+            $('.visible').toggle(300);
+        });
